d83b30dd28edfd29510d47056e95318a1387a380
sanjeev-ramakumar <sanjeev.ramakumar@gmail.com>
2014-07-28 22:37:15 -0700

    tweak home screen ui

--- pregsafe/src/main/java/com/pregsafe/HomeActivity.java
+++ pregsafe/src/main/java/com/pregsafe/HomeActivity.java
@@ -29,6 +29,18 @@ public class HomeActivity extends ActionBarActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+
+        View decorView = getWindow().getDecorView();
+        // hide the status bar
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Hide the action bar
+        getActionBar().hide();
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 0) {
             if (resultCode == RESULT_OK) {
